f227001933046e0d610364c97156e373aa04c2ab
Shny <luciano_258456@hotmail.com>
2025-06-20 16:21:40 -0300

    Arreglar un SP en 04

--- Grupo02/Grupo02/04_ExecProceduresImport.sql
+++ Grupo02/Grupo02/04_ExecProceduresImport.sql
@@ -12,85 +12,17 @@
 --Martins Louro  Luciano DNI:42364189  
 USE Com5600G02
 GO
-----------------------------------------------------------
---				GENERACION DE FACTURAS
-----------------------------------------------------------
-
-CREATE OR ALTER PROCEDURE factura.generar_facturas_mensuales
-    @anio INT = 2024
-AS
-BEGIN
-    SET NOCOUNT ON;
- 
-    DECLARE 
-        @mes INT = 1,
-        @nro_socio VARCHAR(10),
-        @fecha_emision DATE,
-        @fecha_vencimiento DATE,
-		@segunda_fecha_vencimiento DATE,
-        @total NUMERIC(15,2),
-        @nro_socio_num INT;
- 
-    -- Cursor SOLO para socios con nro_socio entre SN-4001 y SN-4120
-    DECLARE cur_socios CURSOR FOR
-        SELECT nro_socio
-        FROM socio.socio
-        WHERE 
-            ISNUMERIC(SUBSTRING(nro_socio, 4, 4)) = 1 AND
-            CAST(SUBSTRING(nro_socio, 4, 4) AS INT) BETWEEN 4001 AND 4120;
- 
-    OPEN cur_socios;
-    FETCH NEXT FROM cur_socios INTO @nro_socio;
- 
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-        SET @mes = 1;
-        SET @nro_socio_num = CAST(SUBSTRING(@nro_socio, 4, 4) AS INT);
- 
-        WHILE @mes <= 12
-        BEGIN
-            -- Fecha de emisión = 1° de cada mes
-            SET @fecha_emision = DATEFROMPARTS(@anio, @mes, 1);
-            -- Fecha de vencimiento = 5 días después
-            SET @fecha_vencimiento = DATEADD(DAY, 5, @fecha_emision);
-			 -- Segunda Fecha de vencimiento = 5 días después
-            SET @segunda_fecha_vencimiento = DATEADD(DAY,10, @fecha_emision);
- 
-            -- Lógica escalonada de montos por bloques
-            SET @total = 
-                CASE 
-                    WHEN @nro_socio_num <= 4040 THEN 15000
-                    WHEN @nro_socio_num <= 4080 THEN 20000
-                    ELSE 22000
-                END;
- 
-            INSERT INTO factura.factura_mensual (fecha_emision, fecha_vencimiento,segunda_fecha_vencimiento, estado, total, nro_socio)
-            VALUES (@fecha_emision, @fecha_vencimiento, @segunda_fecha_vencimiento ,'Pendiente', @total, @nro_socio);
- 
-            SET @mes = @mes + 1;
-        END
- 
-        FETCH NEXT FROM cur_socios INTO @nro_socio;
-    END
- 
-    CLOSE cur_socios;
-    DEALLOCATE cur_socios;
- 
-    PRINT 'Facturas generadas para socios del rango SN-4001 a SN-4120.';
-END;
-EXEC factura.generar_facturas_mensuales @anio = 2024;
-
 
 --=================================================================
 --Importar tabla de OPEN_METEO_BUENOS_AIRES
 --=================================================================
-EXEC factura.importar_clima_csv @path = '[PATH]\open-meteo-buenosaires_2025.csv'
+EXEC factura.importar_clima_csv @path = 'C:\Users\lucia\Desktop\archivos\open-meteo-buenosaires_2025.csv'
 --select * from factura.clima_anual;
 
 --=================================================================
 --Importar tabla de RESPONDABLES DE PAGO
 --=================================================================
-EXEC socio.importar_socios_excel @ruta = '[PATH]\Datos socios.xlsx';
+EXEC socio.importar_socios_excel @ruta = 'C:\Users\lucia\Desktop\archivos\Datos socios.xlsx';
 --	select * from socio.socio_temp
 
 --=================================================================
@@ -109,7 +41,7 @@ EXEC socio.procesar_socios_temp;
 --=================================================================
 --Importar tabla de GRUPO FAMILIAR
 --=================================================================
-EXEC socio.importar_socios_excel2 @ruta = '[PATH]\Datos socios.xlsx';
+EXEC socio.importar_socios_excel2 @ruta = 'C:\Users\lucia\Desktop\archivos\Datos socios.xlsx';
 --select * from socio.socio_temp2
 
 --=================================================================
@@ -132,7 +64,7 @@ delete factura.factura_mensual
 --=================================================================
 --IMPORTAR DE TARIFA E INSERTAR A ACTIVIDAD.ACTIVIDAD 
 --=================================================================
-EXEC actividad.importar_actividades_regulares @path = '[PATH]\Datos socios.xlsx';
+EXEC actividad.importar_actividades_regulares @path = 'C:\Users\lucia\Desktop\archivos\Datos socios.xlsx';
 
 /*
 Ejecutar en caso de que sea necesario modificar la actividad de ajedrez
@@ -158,13 +90,13 @@ select * from actividad.actividad
 --=================================================================
 --IMPORTAR TARIFA E INSERTAR A ACTIVIDAD.ACTIVIDAD_EXTRA (Pileta)
 --=================================================================
-EXEC actividad.importar_tarifas_pileta @path_archivo = '[PATH]\Datos socios.xlsx';
+EXEC actividad.importar_tarifas_pileta @path_archivo = 'C:\Users\lucia\Desktop\archivos\Datos socios.xlsx';
 --select * from actividad.actividad_extra
 
 ---========================================
 --IMPORTAR PRESENTISMO EXCEL
 ---=========================================
-EXEC actividad.importar_presentismo_excel @ruta='[PATH]\Datos socios.xlsx';
+EXEC actividad.importar_presentismo_excel @ruta='C:\Users\lucia\Desktop\archivos\Datos socios.xlsx';
 --select* from ##PresentismoExcel
 
 ---========================================
