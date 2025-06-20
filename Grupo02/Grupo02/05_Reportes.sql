18c4429edcf1e103aafb87d195f459ba75d21706
Shny <luciano_258456@hotmail.com>
2025-06-20 01:28:24 -0300

    Reporte 4

--- Grupo02/Grupo02/05_Reportes.sql
+++ Grupo02/Grupo02/05_Reportes.sql
@@ -1,4 +1,15 @@
-
+--El archivo .sql con el script debe incluir comentarios donde consten este enunciado, 
+--la fecha de entrega,
+--número de grupo, nombre de la materia, nombres y DNI de los alumnos. Entregar todo en un zip 
+--(observar las pautas para nomenclatura antes expuestas) mediante la sección de prácticas de MIEL. 
+--Solo uno de los miembros del grupo debe hacer la entrega
+-- Fecha entrega 23/05/2025
+--Numero grupo: 02
+--Base De Datos Aplicada
+--Celso quelle Nicolas DNI:44382822
+--Paz Curtet  Facundo DNI:44553403  
+--Contti Abel  DNI:394878489  
+--Martins Louro  Luciano DNI:42364189  
 -----------------------------------------------------------------------------
 -- REPORTE DE LOS SOCIOS MOROSOS (2024)
 -----------------------------------------------------------------------------
@@ -26,6 +37,34 @@ FROM Morosidades
 WHERE total_incumplimientos > 2
 ORDER BY total_incumplimientos DESC;
 
+-----------------------------------------------------------------------------
+-- REPORTE DE SOCIOS AUSENTES EN SUS ACTIVIDADES
+-----------------------------------------------------------------------------
+SELECT 
+    soc.nombre,
+    soc.apellido,
+    DATEDIFF(YEAR, soc.fecha_nacimiento, GETDATE()) AS edad,
+    cat.nombre,
+    act.nombre
+FROM socio.socio soc
+JOIN actividad.inscripcion_actividad ins 
+    ON soc.id_socio = ins.id_socio
+JOIN actividad.actividad act 
+    ON ins.id_actividad = act.id_actividad
+JOIN socio.categoria_socio cat 
+    ON soc.id_categoria = cat.nombre
+WHERE EXISTS (
+    SELECT 1
+    FROM actividad.presentismo p1
+    WHERE p1.id_actividad = ins.id_actividad
+)
+AND NOT EXISTS (
+    SELECT 1
+    FROM actividad.presentismo p2
+    WHERE p2.id_actividad = ins.id_actividad
+      AND p2.id_socio = ins.id_socio
+      AND p2.asistencia = 'P'
+);
 
 -----------------------------------------------------------------------------
 -- REPORTE DE INGRESOS DE ACTIVIDADES (DESDE ENERO)
